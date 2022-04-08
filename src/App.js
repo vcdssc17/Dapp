8920b33243812bd20a27aab84f0c9ff1da483b3e
SulimanMokhtar <SulimanMokhtar1995@gmail.com>
2022-04-08 22:31:13 +0200

    Enhance

--- src/App.js
+++ src/App.js
@@ -249,13 +249,13 @@ function App() {
             >
               <StyledButton
                 onClick={(e) => {
-                  window.open("/config/roadmap.pdf", "_blank");
+                  window.open("https://discord.com", "_blank");
                 }}
                 style={{
                   margin: "5px",
                 }}
               >
-                Roadmap
+                Discord
               </StyledButton>
               <StyledButton
                 style={{
