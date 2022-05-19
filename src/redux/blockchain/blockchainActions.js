cd459c22c64a903557ee3131468f254f989297c6
SulimanMokhtar <SulimanMokhtar1995@gmail.com>
2022-05-19 16:12:16 +0200

    Adding Coinbase

--- src/redux/blockchain/blockchainActions.js
+++ src/redux/blockchain/blockchainActions.js
@@ -1,6 +1,9 @@
 // constants
 import Web3EthContract from "web3-eth-contract";
 import Web3 from "web3";
+import Web3Modal from "web3modal";
+import CoinbaseWalletSDK from "@coinbase/wallet-sdk";
+
 // log
 import { fetchData } from "../data/dataActions";
 import blockchainReducer from "./blockchainReducer";
@@ -49,49 +52,86 @@ export const connect = () => {
         Accept: "application/json",
       },
     });
-    const CONFIG = await configResponse.json();
-    const { ethereum } = window;
-    const metamaskIsInstalled = ethereum && ethereum.isMetaMask;
-    if (metamaskIsInstalled) {
-      Web3EthContract.setProvider(ethereum);
-      let web3 = new Web3(ethereum);
-      try {
-        const accounts = await ethereum.request({
-          method: "eth_requestAccounts",
-        });
-        const networkId = await ethereum.request({
-          method: "net_version",
-        });
-        if (networkId == CONFIG.NETWORK.ID) {
-          const SmartContractObj = new Web3EthContract(
-            abi,
-            CONFIG.CONTRACT_ADDRESS
-          );
-          dispatch(
-            connectSuccess({
-              account: accounts[0],
-              smartContract: SmartContractObj,
-              web3: web3,
-            })
-          );
 
-          // Add listeners start
-          ethereum.on("accountsChanged", (accounts) => {
-            dispatch(updateAccount(accounts[0]));
-          });
-          ethereum.on("chainChanged", () => {
-            window.location.reload();
-          });
-          // Add listeners end
-        } else {
-          dispatch(connectFailed(`Change network to ${CONFIG.NETWORK.NAME}.`));
+    const providerOptions = {
+      coinbasewallet: {
+        package: CoinbaseWalletSDK,
+        options: {
+          appName: "Ruleks Web3",
+          infuraId: "9aa3d95b3bc440fa88ea12eaa4456161"
+          // https://mainnet.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161
         }
-      } catch (err) {
-        dispatch(connectFailed("Something went wrong."));
       }
-    } else {
-      dispatch(connectFailed("Install Metamask."));
+    };
+
+    const web3Modal = new Web3Modal({
+      network: "mainnet", // optional
+      cacheProvider: true, // optional
+      // theme: "dark", // optional
+      providerOptions // required
+
+    });
+
+    try {
+      const provider = await web3Modal.connect();
+      console.log(provider);
+
+      const CONFIG = await configResponse.json();
+      if (window.ethereum) {
+        //   Web3EthContract.setProvider(provider);
+        //   let web3 = new Web3(ethereum);
+
+        Web3EthContract.setProvider(provider);
+        let web3 = new Web3(provider);
+        try {
+          // const accounts = await ethereum.request({
+          //   method: "eth_requestAccounts",
+          // });
+          const accounts = await web3.eth.getAccounts();
+
+          // const networkId = await ethereum.request({
+          //   method: "net_version",
+          // });
+
+          const networkId = await web3.eth.net.getId();
+
+          if (networkId == CONFIG.NETWORK.ID) {
+            const SmartContractObj = new Web3EthContract(
+              abi,
+              CONFIG.CONTRACT_ADDRESS
+            );
+            dispatch(
+              connectSuccess({
+                account: accounts[0],
+                smartContract: SmartContractObj,
+                web3: web3,
+              })
+            );
+
+            //       // Add listeners start
+            provider.on("accountsChanged", (accounts) => {
+              dispatch(updateAccount(accounts[0]));
+            });
+            provider.on("chainChanged", () => {
+              window.location.reload();
+            });
+            //       // Add listeners end
+          } else {
+            dispatch(connectFailed(`Change network to ${CONFIG.NETWORK.NAME}.`));
+          }
+        } catch (err) {
+          dispatch(connectFailed("Something went wrong."));
+        }
+      } else {
+        dispatch(connectFailed("Install Metamask or Coinbase."));
+      }
+    } catch (err) {
+      dispatch(connectFailed("Something went wrong."));
+
     }
+
+
+
   };
 };
 
