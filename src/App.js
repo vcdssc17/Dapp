ae1f1d305b59f8e8905b1d4af3d922a7776b2511
Aleks <vcds@gmx.at>
2022-05-20 12:53:04 +0200

    update max per wallet

--- src/App.js
+++ src/App.js
@@ -256,8 +256,8 @@ function App() {
 
   const incrementMintAmount = () => {
     let newMintAmount = mintAmount + 1;
-    if (newMintAmount > 50) {
-      newMintAmount = 50;
+    if (newMintAmount > 10) {
+      newMintAmount = 10;
     }
     setMintAmount(newMintAmount);
   };
